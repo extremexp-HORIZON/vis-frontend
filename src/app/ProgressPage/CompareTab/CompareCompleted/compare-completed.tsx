c851d6dd80049bbda5901caa78188e81b6d8efe1
Konstantinos Kozanis <kon.kozanis@gmail.com>
2024-07-11 12:22:02 +0300

    added top-level svg to comparison page

--- src/app/ProgressPage/CompareTab/CompareCompleted/compare-completed.tsx
+++ src/app/ProgressPage/CompareTab/CompareCompleted/compare-completed.tsx
@@ -4,41 +4,88 @@ import TopWorkflowMetric from "./top-workflow-metric"
 import { Grid, Typography } from "@mui/material"
 import VariabilityPointCharts from "./variability-point-charts"
 import VariabilityPointHeatmap from "./variability-point-heatmap"
+import CompareCompletedSvg from "./compare-completed-svg"
+import { useState } from "react"
 
 const CompareCompleted = () => {
+  const [chosenTask, setChosenTask] = useState<string | null>(null)
   return (
     <Box
       key="compare-completed"
       sx={{ display: "flex", flexDirection: "column", rowGap: 2 }}
     >
-      <Box key="compare-completed-title">
-        <Typography
-          variant="body1"
-          sx={{ fontWeight: 600, fontSize: "1.5rem" }}
-        >
-          Metrics Analysis
-        </Typography>
+      <Box key="compare-completed-svg">
+        <CompareCompletedSvg
+          setChosenTask={setChosenTask}
+          chosenTask={chosenTask}
+        />
       </Box>
-      <Box
-        key="compare-completed-items"
-        sx={{ display: "flex", flexDirection: "column", rowGap: 2, mb: 1 }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <MetricsDistribution />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TopWorkflowMetric />
-          </Grid>
-          
-        </Grid>
-        <Box>
-            <VariabilityPointCharts />
+      {!chosenTask ? (
+        <>
+          <Box
+            key="compare-completed-metrics-analysis"
+            sx={{ display: "flex", flexDirection: "column", rowGap: 2 }}
+          >
+            <Box key="compare-completed-metrics-analysis-title">
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: 600, fontSize: "1.5rem" }}
+              >
+                Metrics Analysis
+              </Typography>
+            </Box>
+            <Box
+              key="compare-completed--metrics-analysis-items"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                rowGap: 2,
+                mb: 1,
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <MetricsDistribution />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TopWorkflowMetric />
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-          <Box>
-            <VariabilityPointHeatmap />
+          <Box
+            key="compare-completed-variability-point-analysis"
+            sx={{ display: "flex", flexDirection: "column", rowGap: 2 }}
+          >
+            <Box key="compare-completed-metrics-analysis-title">
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: 600, fontSize: "1.5rem" }}
+              >
+                Variability Point Analysis
+              </Typography>
+            </Box>
+            <Box
+              key="compare-completed-variability-point-analysis-items"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                rowGap: 2,
+                mb: 1,
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <VariabilityPointCharts />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <VariabilityPointHeatmap />
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-      </Box>
+        </>
+      ) : null}
     </Box>
   )
 }
--- src/app/ProgressPage/CompareTab/CompareCompleted/metrics-distribution.tsx
+++ src/app/ProgressPage/CompareTab/CompareCompleted/metrics-distribution.tsx
@@ -11,7 +11,6 @@ import {
   IconButton,
   Paper,
   Tooltip,
-  Grid,
 } from "@mui/material"
 import { grey } from "@mui/material/colors"
 import InfoIcon from "@mui/icons-material/Info"
@@ -43,173 +42,8 @@ const MetricsDistribution = () => {
     { metricName: "runtime", value: 1.1 },
   ]
 
-  const generateSpecForMetric = (metric: any) => {
-    const filteredData = data.filter(d => d.metricName === metric)
-    return {
-      width: 300,
-      height: 200,
-      padding: 5,
-      config: {
-        axisBand: {
-          bandPosition: 1,
-          tickExtra: false,
-          tickOffset: 0,
-        },
-      },
-      signals: [
-        { name: "plotWidth", update: "(width - 50)/3" },
-        { name: "height", update: "height" },
-        { name: "trim", value: false }, // Default value set to false
-        { name: "bandwidth", value: 0.1 }, // Default value set to 0
-      ],
-      data: [
-        {
-          name: "dummyData",
-          values: filteredData,
-        },
-        {
-          name: "density",
-          source: "dummyData",
-          transform: [
-            {
-              type: "kde",
-              field: "value",
-              groupby: ["metricName"],
-              bandwidth: { signal: "bandwidth" },
-              extent: { signal: "trim ? null : [0, 2]" },
-            },
-          ],
-        },
-        {
-          name: "stats",
-          source: "dummyData",
-          transform: [
-            {
-              type: "aggregate",
-              groupby: ["metricName"],
-              fields: ["value", "value", "value"],
-              ops: ["q1", "median", "q3"],
-              as: ["q1", "median", "q3"],
-            },
-          ],
-        },
-      ],
-      scales: [
-        {
-          name: "layout",
-          type: "band",
-          range: "width",
-          domain: { data: "dummyData", field: "metricName" },
-        },
-        {
-          name: "yscale",
-          type: "linear",
-          range: "height",
-          round: true,
-          domain: { data: "dummyData", field: "value" },
-          domainMin: 0,
-          domainMax: 1.5,
-
-          zero: false,
-          nice: true,
-          reverse: false,
-        },
-        {
-          name: "hscale",
-          type: "linear",
-          range: [0, { signal: "plotWidth" }],
-          domain: { data: "density", field: "density" },
-        },
-        {
-          name: "color",
-          type: "ordinal",
-          domain: { data: "dummyData", field: "metricName" },
-          range: "category",
-        },
-      ],
-      axes: [
-        { orient: "bottom", scale: "layout", zindex: 1 },
-        { orient: "left", scale: "yscale", zindex: 1 },
-      ],
-      marks: [
-        {
-          type: "group",
-          from: {
-            facet: {
-              data: "density",
-              name: "violin",
-              groupby: "metricName",
-            },
-          },
-          encode: {
-            enter: {
-              xc: { scale: "layout", field: "metricName", band: 0.5 },
-              width: { signal: "plotWidth" },
-              height: { signal: "height" },
-            },
-          },
-          data: [
-            {
-              name: "summary",
-              source: "stats",
-              transform: [
-                {
-                  type: "filter",
-                  expr: "datum.metricName === parent.metricName",
-                },
-              ],
-            },
-          ],
-          marks: [
-            {
-              type: "area",
-              from: { data: "violin" },
-              encode: {
-                enter: {
-                  fill: { scale: "color", field: { parent: "metricName" } },
-                  orient: { value: "horizontal" },
-                },
-                update: {
-                  y: { scale: "yscale", field: "value" },
-                  xc: { signal: "plotWidth / 2" },
-                  width: { scale: "hscale", field: "density" },
-                },
-              },
-            },
-            {
-              type: "rect",
-              from: { data: "summary" },
-              encode: {
-                enter: {
-                  fill: { value: "black" },
-                  width: { value: 5 },
-                },
-                update: {
-                  y: { scale: "yscale", field: "q1" },
-                  y2: { scale: "yscale", field: "q3" },
-                  xc: { signal: "plotWidth / 2" },
-                },
-              },
-            },
-            {
-              type: "rect",
-              from: { data: "summary" },
-              encode: {
-                enter: {
-                  fill: { value: "black" },
-                  height: { value: 20 },
-                  width: { value: 20 },
-                },
-                update: {
-                  y: { scale: "yscale", field: "median" },
-                  xc: { signal: "plotWidth / 2" },
-                },
-              },
-            },
-          ],
-        },
-      ],
-    }
+  const getFilteredData = (metric: any) => {
+    return  data.filter(d => d.metricName === metric)
   }
 
   return (
@@ -245,41 +79,210 @@ const MetricsDistribution = () => {
           </IconButton>
         </Tooltip>
       </Box>
-        <Box sx={{ display: "flex", alignItems: "center", px: 1.5 }}>
-          <Typography fontSize={"0.8rem"}>Metrics:</Typography>
-          <FormControl
-            sx={{ m: 1, minWidth: 120, maxHeight: 120 }}
-            size="small"
+      <Box sx={{ display: "flex", alignItems: "center", px: 1.5 }}>
+        <Typography fontSize={"0.8rem"}>Metrics:</Typography>
+        <FormControl sx={{ m: 1, minWidth: 120, maxHeight: 120 }} size="small">
+          <Select
+            labelId="metric-select-label"
+            multiple
+            value={selectedMetrics}
+            onChange={handleMetricChange}
+            renderValue={selected => (selected as string[]).join(", ")}
           >
-            <Select
-              labelId="metric-select-label"
-              multiple
-              value={selectedMetrics}
-              onChange={handleMetricChange}
-              renderValue={selected => (selected as string[]).join(", ")}
-            >
-              {metrics.map(metric => (
-                <MenuItem key={metric} value={metric}>
-                  <Checkbox checked={selectedMetrics.indexOf(metric) > -1} />
-                  <ListItemText primary={metric} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Box sx={{ width: "99%", px: 1, display: "flex", justifyContent: "center", height: "100%", flexWrap: "wrap" }}>
-            {/* <Grid container spacing={2} sx={{width: "100%"}}> */}
+            {metrics.map(metric => (
+              <MenuItem key={metric} value={metric}>
+                <Checkbox checked={selectedMetrics.indexOf(metric) > -1} />
+                <ListItemText primary={metric} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box
+        sx={{
+          width: "99%",
+          px: 1,
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          flexWrap: "wrap",
+        }}
+      >
         {selectedMetrics.map(metric => (
-            // <Grid item xs={12} sm={3} lg={3}>
-            <Vega
-              key={`metric-${metric}-distribution`}
-              actions={false}
-              style={{ width: "max-content" }}
-              spec={generateSpecForMetric(metric)}
-              />
-        //   </Grid>
+          <VegaLite
+            key={`metric-${metric}-distribution`}
+            actions={false}
+            style={{ width: "max-content" }}
+            spec={{
+              width: 300,
+              height: 200,
+              padding: 5,
+              config: {
+                axisBand: {
+                  bandPosition: 1,
+                  tickExtra: false,
+                  tickOffset: 0,
+                },
+              },
+              signals: [
+                { name: "plotWidth", update: "(width - 50)/3" },
+                { name: "height", update: "height" },
+                { name: "trim", value: false }, // Default value set to false
+                { name: "bandwidth", value: 0.1 }, // Default value set to 0
+              ],
+              data: [
+                {
+                  name: "dummyData",
+                  values: getFilteredData(metric),
+                },
+                {
+                  name: "density",
+                  source: "dummyData",
+                  transform: [
+                    {
+                      type: "kde",
+                      field: "value",
+                      groupby: ["metricName"],
+                      bandwidth: { signal: "bandwidth" },
+                      extent: { signal: "trim ? null : [0, 2]" },
+                    },
+                  ],
+                },
+                {
+                  name: "stats",
+                  source: "dummyData",
+                  transform: [
+                    {
+                      type: "aggregate",
+                      groupby: ["metricName"],
+                      fields: ["value", "value", "value"],
+                      ops: ["q1", "median", "q3"],
+                      as: ["q1", "median", "q3"],
+                    },
+                  ],
+                },
+              ],
+              scales: [
+                {
+                  name: "layout",
+                  type: "band",
+                  range: "width",
+                  domain: { data: "dummyData", field: "metricName" },
+                },
+                {
+                  name: "yscale",
+                  type: "linear",
+                  range: "height",
+                  round: true,
+                  domain: { data: "dummyData", field: "value" },
+                  domainMin: 0,
+                  domainMax: 1.5,
+
+                  zero: false,
+                  nice: true,
+                  reverse: false,
+                },
+                {
+                  name: "hscale",
+                  type: "linear",
+                  range: [0, { signal: "plotWidth" }],
+                  domain: { data: "density", field: "density" },
+                },
+                {
+                  name: "color",
+                  type: "ordinal",
+                  domain: { data: "dummyData", field: "metricName" },
+                  range: "category",
+                },
+              ],
+              axes: [
+                { orient: "bottom", scale: "layout", zindex: 1 },
+                { orient: "left", scale: "yscale", zindex: 1 },
+              ],
+              marks: [
+                {
+                  type: "group",
+                  from: {
+                    facet: {
+                      data: "density",
+                      name: "violin",
+                      groupby: "metricName",
+                    },
+                  },
+                  encode: {
+                    enter: {
+                      xc: { scale: "layout", field: "metricName", band: 0.5 },
+                      width: { signal: "plotWidth" },
+                      height: { signal: "height" },
+                    },
+                  },
+                  data: [
+                    {
+                      name: "summary",
+                      source: "stats",
+                      transform: [
+                        {
+                          type: "filter",
+                          expr: "datum.metricName === parent.metricName",
+                        },
+                      ],
+                    },
+                  ],
+                  marks: [
+                    {
+                      type: "area",
+                      from: { data: "violin" },
+                      encode: {
+                        enter: {
+                          fill: {
+                            scale: "color",
+                            field: { parent: "metricName" },
+                          },
+                          orient: { value: "horizontal" },
+                        },
+                        update: {
+                          y: { scale: "yscale", field: "value" },
+                          xc: { signal: "plotWidth / 2" },
+                          width: { scale: "hscale", field: "density" },
+                        },
+                      },
+                    },
+                    {
+                      type: "rect",
+                      from: { data: "summary" },
+                      encode: {
+                        enter: {
+                          fill: { value: "black" },
+                          width: { value: 5 },
+                        },
+                        update: {
+                          y: { scale: "yscale", field: "q1" },
+                          y2: { scale: "yscale", field: "q3" },
+                          xc: { signal: "plotWidth / 2" },
+                        },
+                      },
+                    },
+                    {
+                      type: "rect",
+                      from: { data: "summary" },
+                      encode: {
+                        enter: {
+                          fill: { value: "black" },
+                          height: { value: 20 },
+                          width: { value: 20 },
+                        },
+                        update: {
+                          y: { scale: "yscale", field: "median" },
+                          xc: { signal: "plotWidth / 2" },
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            }}
+          />
         ))}
-        {/* </Grid> */}
       </Box>
     </Paper>
   )
