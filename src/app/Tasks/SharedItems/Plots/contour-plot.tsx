af116874bdc65b5139d072e80a54bdb7b1ee61c1
Konstantinos Kozanis <kon.kozanis@gmail.com>
2025-01-16 10:49:33 +0200

    Add default payload for explainability plot in ContourPlot component

--- src/app/Tasks/SharedItems/Plots/contour-plot.tsx
+++ src/app/Tasks/SharedItems/Plots/contour-plot.tsx
@@ -14,6 +14,7 @@ import { IPlotModel } from "../../../../shared/models/plotmodel.model"
 import { useAppDispatch } from "../../../../store/store"
 import { AsyncThunk } from "@reduxjs/toolkit"
 import CircularProgress from "@mui/material/CircularProgress"
+import { fetchExplainabilityPlotPayloadDefault } from "../../../../shared/models/tasks/explainability.model"
 
 interface IContourplot {
   plotModel: {
@@ -69,6 +70,7 @@ const ContourPlot = (props: IContourplot) => {
     (e: { target: { value: string } }) => {
       dispatch(
         fetchFunction({
+          ...fetchExplainabilityPlotPayloadDefault,
           explanationType: plmodel?.explainabilityType || "",
           explanationMethod: plmodel?.explanationMethod || "",
           model: plmodel?.explainabilityModel || "",
