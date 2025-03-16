c96c93689beb6635373c32aec53d9b76f16887c1
pgidar <pgidar@athenarc.gr>
2025-03-16 23:14:05 +0200

    workflow page, added pause stop and progress bar!

--- src/app/ProgressPage/WorkflowTab/workflow-tab.tsx
+++ src/app/ProgressPage/WorkflowTab/workflow-tab.tsx
@@ -5,9 +5,10 @@ import { RootState, useAppSelector } from "../../../store/store"
 import Rating from "@mui/material/Rating"
 import { useNavigate } from "react-router-dom"
 import ArrowBackIcon from "@mui/icons-material/ArrowBack"
-import { Tabs, Tab, Card } from "@mui/material"
+import { Tabs, Tab, Card, CircularProgress } from "@mui/material"
 import DataExplorationComponent from "../../Tasks/DataExplorationTask/ComponentContainer/DataExplorationComponent"
-
+import PauseIcon from "@mui/icons-material/Pause"
+import StopIcon from "@mui/icons-material/Stop"
 interface IWorkflowTab {
   workflowId: number | string
 }
@@ -15,34 +16,27 @@ interface IWorkflowTab {
 const WorkflowTab = (props: IWorkflowTab) => {
   const { workflowId } = props
   const { tabs } = useAppSelector((state: RootState) => state.workflowTabs)
-  const [chosenTask, setChosenTask] = useState<string | null>(null)
   const { workflows, progressBar } = useAppSelector(
     (state: RootState) => state.progressPage,
   )
   const selectedTab = tabs.find(tab => tab.workflowId === workflowId)
   const navigate = useNavigate()
   const [selectedTabs, setSelectedTabs] = useState(0)
+  console.log("progressBar:", progressBar)
 
-  console.log("tabs",tabs)
-  console.log("workflow",workflows)
-  console.log("sugkekrimeno workflow", workflows.data.find(workflow => workflow.workflowId === workflowId))
-  const workflow = workflows.data.find(workflow => workflow.workflowId === workflowId);
-  const metrics = workflow ? workflow.metrics : [];
-
-  console.log("pinwmauro",workflows.data.find(workflow => workflow.workflowId === workflowId)?.metrics)
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column", rowGap: 2, mb: 3 }}>
-        <Box
-          key="workflow-title"
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            columnGap: 2,
-            justifyContent: "left",
-            alignItems: "center",
-          }}
-        >
+      <Box
+        key="workflow-title"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          columnGap: 2,
+          justifyContent: "space-between", // Ensures left and right alignment
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
           <ArrowBackIcon
             fontSize="large"
             sx={{
@@ -74,82 +68,136 @@ const WorkflowTab = (props: IWorkflowTab) => {
           />
         </Box>
 
-        <Tabs
-          value={selectedTabs}
-          onChange={(event, newValue) => setSelectedTabs(newValue)}
-          aria-label="tab menu"
-        >
-          <Tab
-            sx={{
-              fontWeight: 600,
-              color: selectedTabs === 0 ? "black" : "gray",
-            }}
-            label="METRICS"
-          />
-          <Tab
-            sx={{
-              fontWeight: 600,
-              color: selectedTabs === 1 ? "black" : "gray",
-            }}
-            label="DATA EXPLORATION"
-          />
-          <Tab
-            sx={{
-              fontWeight: 600,
-              color: selectedTabs === 2 ? "black" : "gray",
-            }}
-            label="SOURCE CODE"
-          />
-          <Tab
-            sx={{
-              fontWeight: 600,
-              color: selectedTabs === 3 ? "black" : "gray",
-            }}
-            label="MONITORING"
-          />
-          <Tab
-            sx={{
-              fontWeight: 600,
-              color: selectedTabs === 4 ? "black" : "gray",
-            }}
-            label="USER INPUT"
-          />
-        </Tabs>
-
-        {/* Tab Content */}
-        <Box sx={{ p: 2 }}>
-        {selectedTabs === 0 && (
-  <Box sx={{ display: "flex", flexDirection: "row", gap: 2, justifyContent: "space-between", width: "100%" }}>
-    {workflows.data.find(workflow => workflow.workflowId === workflowId)?.metrics.map((metric) => (
-      <Card 
-        key={metric.metricId} 
-        sx={{ 
-          flex: 1, // Ensures cards take equal space
-          textAlign: "center", 
-          backgroundColor: "#f5f5f5", 
-          boxShadow: 2 
-        }}
-      >
-        <Typography variant="body1" sx={{ fontWeight: 200 }}>
-          {metric.name}
-        </Typography>
-        <Typography variant="h6" sx={{ color: "blue" }}>
-          {metric.value.toFixed(3)}
-        </Typography>
-      </Card>
-    ))}
-  </Box>
-)}
-
-          {selectedTabs === 1 && (
-            <DataExplorationComponent
-              workflow={tabs.find(tab => tab.workflowId === workflowId) || null}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Box sx={{ position: "relative", display: "inline-flex" }}>
+            <CircularProgress
+              variant="determinate"
+              value={progressBar.progress}
+              size={50}
+              thickness={5}
+              sx={{ color: theme => theme.palette.primary.main }}
             />
-          )}
-          {selectedTabs === 2 && <Typography>Source Code Content</Typography>}
-          {selectedTabs === 3 && <Typography>Montiroring Content</Typography>}
-          {selectedTabs === 4 && <Typography>User Input Content</Typography>}
+            <Box
+              sx={{
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                position: "absolute",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontWeight: "bold",
+                fontSize: "1rem",
+              }}
+            >
+              {`${Math.round(progressBar.progress)}%`}
+            </Box>
+          </Box>
+          <PauseIcon
+            onClick={() => console.log("clicked")}
+            sx={{
+              cursor: "pointer",
+              color: theme => theme.palette.primary.main,
+            }}
+            fontSize="large"
+          />
+          <StopIcon
+            onClick={() => console.log("clicked")}
+            sx={{
+              cursor: "pointer",
+              color: theme => theme.palette.primary.main,
+            }}
+            fontSize="large"
+          />
         </Box>
+      </Box>
+
+      <Tabs
+        value={selectedTabs}
+        onChange={(event, newValue) => setSelectedTabs(newValue)}
+        aria-label="tab menu"
+      >
+        <Tab
+          sx={{
+            fontWeight: 600,
+            color: selectedTabs === 0 ? "black" : "gray",
+          }}
+          label="METRICS"
+        />
+        <Tab
+          sx={{
+            fontWeight: 600,
+            color: selectedTabs === 1 ? "black" : "gray",
+          }}
+          label="DATA EXPLORATION"
+        />
+        <Tab
+          sx={{
+            fontWeight: 600,
+            color: selectedTabs === 2 ? "black" : "gray",
+          }}
+          label="SOURCE CODE"
+        />
+        <Tab
+          sx={{
+            fontWeight: 600,
+            color: selectedTabs === 3 ? "black" : "gray",
+          }}
+          label="MONITORING"
+        />
+        <Tab
+          sx={{
+            fontWeight: 600,
+            color: selectedTabs === 4 ? "black" : "gray",
+          }}
+          label="USER INPUT"
+        />
+      </Tabs>
+
+      {/* Tab Content */}
+      <Box sx={{ p: 2 }}>
+        {selectedTabs === 0 && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 2,
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            {workflows.data
+              .find(workflow => workflow.workflowId === workflowId)
+              ?.metrics.map(metric => (
+                <Card
+                  key={metric.metricId}
+                  sx={{
+                    flex: 1, // Ensures cards take equal space
+                    textAlign: "center",
+                    backgroundColor: "#f5f5f5",
+                    boxShadow: 2,
+                  }}
+                >
+                  <Typography variant="body1" sx={{ fontWeight: 200 }}>
+                    {metric.name}
+                  </Typography>
+                  <Typography variant="h6" sx={{ color: "blue" }}>
+                    {metric.value}
+                  </Typography>
+                </Card>
+              ))}
+          </Box>
+        )}
+
+        {selectedTabs === 1 && (
+          <DataExplorationComponent
+            workflow={tabs.find(tab => tab.workflowId === workflowId) || null}
+          />
+        )}
+        {selectedTabs === 2 && <Typography>Source Code Content</Typography>}
+        {selectedTabs === 3 && <Typography>Montiroring Content</Typography>}
+        {selectedTabs === 4 && <Typography>User Input Content</Typography>}
       </Box>
     </>
   )
