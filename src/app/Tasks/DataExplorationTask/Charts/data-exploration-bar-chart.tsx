a614a66fcb8ee9148564e97c47dcfae0364ca0f7
PanosGidarakos <panosgidarakos@gmail.com>
2025-05-08 16:36:30 +0200

    Refactor BarChart component for improved readability and maintainability

--- src/app/Tasks/DataExplorationTask/Charts/data-exploration-bar-chart.tsx
+++ src/app/Tasks/DataExplorationTask/Charts/data-exploration-bar-chart.tsx
@@ -11,46 +11,50 @@ import { defaultDataExplorationQuery } from "../../../../shared/models/dataexplo
 // Assuming dataExploration is passed as a prop or obtained from elsewhere
 const BarChart = () => {
   const dispatch = useAppDispatch()
-  const {tab} = useAppSelector(state => state.workflowPage)
-    const theme = useTheme()
-    
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("xl"))
-    useEffect(() => {
-      const groupBy = tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy
-      const aggregation = tab?.workflowTasks.dataExploration?.controlPanel.barAggregation
-      const datasetId = tab?.dataTaskTable.selectedItem?.data?.source || ""
-      const filters = tab?.workflowTasks.dataExploration?.controlPanel.filters
-    
-      if (!datasetId || !groupBy?.length || !Object.keys(aggregation || {}).length ) {
-        return // Don't dispatch if missing dataset, groupBy, or aggregation
-      }
-    
-      dispatch(
-        fetchDataExplorationData({
-          query: {
-            ...defaultDataExplorationQuery,
-            datasetId,
-            groupBy,
-            aggregation,
-            filters
-          },
-          metadata: {
-            workflowId: tab?.workflowId || "",
-            queryCase: "barChart",
-          },
-        })
-      )
-    }, [
-      tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy,
-      tab?.workflowTasks.dataExploration?.controlPanel.barAggregation,
-      tab?.dataTaskTable.selectedItem?.data?.source,
-      tab?.workflowTasks.dataExploration?.controlPanel.filters
+  const { tab } = useAppSelector(state => state.workflowPage)
+  const theme = useTheme()
 
-    ])
-    
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("xl"))
+  useEffect(() => {
+    const groupBy = tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy
+    const aggregation =
+      tab?.workflowTasks.dataExploration?.controlPanel.barAggregation
+    const datasetId = tab?.dataTaskTable.selectedItem?.data?.source || ""
+    const filters = tab?.workflowTasks.dataExploration?.controlPanel.filters
 
-  
-  
+    if (
+      !datasetId ||
+      !groupBy?.length ||
+      !Object.keys(aggregation || {}).length
+    ) {
+      return // Don't dispatch if missing dataset, groupBy, or aggregation
+    }
+    dispatch(
+      fetchDataExplorationData({
+        query: {
+          ...defaultDataExplorationQuery,
+          datasetId,
+          groupBy,
+          aggregation,
+          filters,
+        },
+        metadata: {
+          workflowId: tab?.workflowId || "",
+          queryCase: "barChart",
+        },
+      }),
+    )
+  }, [
+    tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy,
+    tab?.workflowTasks.dataExploration?.controlPanel.barAggregation,
+    tab?.dataTaskTable.selectedItem?.data?.source,
+    tab?.workflowTasks.dataExploration?.controlPanel.filters,
+  ])
+
+  console.log(
+    "bar agg",
+    tab?.workflowTasks.dataExploration?.controlPanel.barAggregation,
+  )
 
   const columns = tab?.workflowTasks.dataExploration?.barChart.data?.columns
   const xAxisColumn = columns?.find(col => col.type === "STRING")?.name
@@ -62,20 +66,26 @@ const BarChart = () => {
     .map(col => col.name)
 
   // Transform the data into a suitable format for grouped bar chart
-  const transformedData = tab?.workflowTasks.dataExploration?.barChart.data?.data.flatMap((item: { [x: string]: any }) =>
-    yAxisColumns?.map(col => ({
-      [xAxisColumn as string]: item[xAxisColumn as string],
-      type: col, // Each numeric column becomes a type/category
-      value: item[col], // The value for each column
-      ...Object.fromEntries(
-        (categoricalColumns || []).map(catCol => [catCol.name, item[catCol.name]]),
-      ), // Include all categorical values
-    })),
-  )
+  const transformedData =
+    tab?.workflowTasks.dataExploration?.barChart.data?.data.flatMap(
+      (item: { [x: string]: any }) =>
+        yAxisColumns?.map(col => ({
+          [xAxisColumn as string]: item[xAxisColumn as string],
+          type: col, // Each numeric column becomes a type/category
+          value: item[col], // The value for each column
+          ...Object.fromEntries(
+            (categoricalColumns || []).map(catCol => [
+              catCol.name,
+              item[catCol.name],
+            ]),
+          ), // Include all categorical values
+        })),
+    )
 
   const specification = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-    description: "A grouped bar chart showing different numeric values by category.",
+    description:
+      "A grouped bar chart showing different numeric values by category.",
     autosize: { type: "fit", contains: "padding", resize: true },
     data: { values: transformedData },
     mark: "bar",
@@ -107,9 +117,8 @@ const BarChart = () => {
         field: "type",
         type: "nominal",
         title: "Metric",
-        // legend: 
+        // legend:
         //   null
-        
       },
       xOffset: {
         field: "type",
@@ -132,29 +141,32 @@ const BarChart = () => {
     },
   }
 
-  
-
   const info = (
     <InfoMessage
       message="Please select both Group By and Aggregation to display the chart."
-
       type="info"
       icon={<AssessmentIcon sx={{ fontSize: 40, color: "info.main" }} />}
       fullHeight
-  />
-  
+    />
   )
-  const shouldShowInfoMessage =
-  tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy.length === 0 || Object.keys(tab?.workflowTasks.dataExploration?.controlPanel.barAggregation).length === 0
+  const hasValidAggregation = Object.values(
+    tab?.workflowTasks.dataExploration?.controlPanel.barAggregation || {},
+  ).some((val: any) => Array.isArray(val) && val.length > 0)
+
+  const hasGroupBy =
+    (tab?.workflowTasks.dataExploration?.controlPanel.barGroupBy || []).length >
+    0
+
+  const shouldShowInfoMessage = !hasGroupBy || !hasValidAggregation
   return (
-    <Box sx={{height: "99%"}}>
-      <ResponsiveCardVegaLite 
-        spec={specification} 
-        actions={false} 
+    <Box sx={{ height: "99%" }}>
+      <ResponsiveCardVegaLite
+        spec={specification}
+        actions={false}
         title={"Bar Chart"}
         maxHeight={500}
         aspectRatio={isSmallScreen ? 2.8 : 1.8}
-        controlPanel={<BarChartControlPanel/>}
+        controlPanel={<BarChartControlPanel />}
         infoMessage={info}
         showInfoMessage={shouldShowInfoMessage}
         loading={tab?.workflowTasks.dataExploration?.barChart?.loading}
