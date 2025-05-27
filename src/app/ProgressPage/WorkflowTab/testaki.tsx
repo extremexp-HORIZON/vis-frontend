7e33fbf681d1353bfbcda8b59447f1394681c1c6
PanosGidarakos <panosgidarakos@gmail.com>
2025-05-27 14:00:56 +0200

    Refactor fetchCatalogAssets dispatch to use template literals for project_id

--- src/app/ProgressPage/WorkflowTab/testaki.tsx
+++ src/app/ProgressPage/WorkflowTab/testaki.tsx
@@ -45,14 +45,15 @@ const Testaki = () => {
   const isDirectory = !source.includes('.') || source.endsWith('/');
   const [selectedFile, setSelectedFile] = useState<string | null>(null);
   const [metadataDialogOpen, setMetadataDialogOpen] = useState(false);
-  
   const handleOpenMetadataDialog = () => setMetadataDialogOpen(true);
   const handleCloseMetadataDialog = () => setMetadataDialogOpen(false);
-  console.log('selectedItem', selectedItem?.data.dataset?.source);
   const dispatch = useAppDispatch()
+
+  const experimentId='IEwJDZcBpHPS2GeIwzH6';
+  const workflowId='IUwJDZcBpHPS2GeIyjFx';
   useEffect(() => {
     // Dispatch the thunk to fetch catalog assets
-    dispatch(fetchCatalogAssets({ project_id:'TestZenohExp/IEwJDZcBpHPS2GeIwzH6/IUwJDZcBpHPS2GeIyjFx' , page: '1', perPage: '10', sort: 'created,desc' }));
+    dispatch(fetchCatalogAssets({ project_id:`${experimentId}/${workflowId}` , page: '1', perPage: '10', sort: 'created,desc' }));
   }, [dispatch]);
 
   const catalog=tab?.catalogAssets.data || [];
