778e31d878cea0d2fa34421d7f5a2fb67b6cc3e9
PanosGidarakos <panosgidarakos@gmail.com>
2025-06-25 15:56:15 +0200

    feat: add control panel for axis selection in comparison model instance

--- src/app/ProgressPage/MonitoringPage/ModelComparison/comparison-model-instance.tsx
+++ src/app/ProgressPage/MonitoringPage/ModelComparison/comparison-model-instance.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Box, FormControl, Grid, InputLabel, MenuItem, Select, Switch, Typography } from '@mui/material';
 import type { RootState } from '../../../../store/store';
 import { useAppDispatch, useAppSelector } from '../../../../store/store';
 import Loader from '../../../../shared/components/loader';
@@ -6,10 +6,107 @@ import ResponsiveCardTable from '../../../../shared/components/responsive-card-t
 import InfoMessage from '../../../../shared/components/InfoMessage';
 import ResponsiveCardVegaLite from '../../../../shared/components/responsive-card-vegalite';
 import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchComparativeModelInstances } from '../../../../store/slices/monitorPageSlice';
 import type { TestInstance } from '../../../../shared/models/tasks/model-analysis.model';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 
+interface ControlPanelProps {
+  xAxisOption: string
+  yAxisOption: string
+  setXAxisOption: (val: string) => void
+  setYAxisOption: (val: string) => void
+  options: string[]
+  plotData: {
+    data: TestInstance[] | null
+    loading: boolean
+    error: string | null
+  } | null
+  
+}
+
+const ControlPanel = ({
+  xAxisOption,
+  yAxisOption,
+  setXAxisOption,
+  setYAxisOption,
+  options,
+  plotData,
+  
+}: ControlPanelProps) => {
+  const handleAxisSelection =
+    (axis: 'x' | 'y') => (e: { target: { value: string } }) => {
+      if (axis === 'x') setXAxisOption(e.target.value);
+      else setYAxisOption(e.target.value);
+    };
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, px: 1.5 }}>
+        {/* X-Axis Selector */}
+        <FormControl fullWidth>
+          <InputLabel id="x-axis-select-label">
+            <Box display="flex" alignItems="center" gap={1}>
+              <ShowChartIcon fontSize="small" />
+                X-Axis
+            </Box>
+          </InputLabel>
+          <Select
+            labelId="x-axis-select-label"
+            label="X-Axis-----"
+            disabled={plotData?.loading || !plotData?.data}
+            value={xAxisOption}
+            onChange={handleAxisSelection('x')}
+            MenuProps={{
+              PaperProps: { style: { maxHeight: 224, width: 250 } },
+            }}
+          >
+            {options
+              .filter(option => option !== yAxisOption)
+              .map((feature, idx) => (
+                <MenuItem key={`xAxis-${feature}-${idx}`} value={feature}>
+                  {feature}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="y-axis-select-label">
+            <Box display="flex" alignItems="center" gap={1}>
+              <ShowChartIcon fontSize="small" />
+                Y-Axis
+            </Box>
+          </InputLabel>
+          <Select
+            labelId="y-axis-select-label"
+            label="Y-Axis-----"
+            disabled={plotData?.loading || !plotData?.data}
+            value={yAxisOption}
+            onChange={handleAxisSelection('y')}
+            MenuProps={{
+              PaperProps: { style: { maxHeight: 224, width: 250 } },
+            }}
+          >
+            {options
+              .filter(option => option !== xAxisOption)
+              .map((feature, idx) => (
+                <MenuItem key={`yAxis-${feature}-${idx}`} value={feature}>
+                  {feature}
+                </MenuItem>
+              ))}
+
+            {options.filter(option => option !== xAxisOption).length === 0 && (
+              <MenuItem disabled value="">
+                No available options
+              </MenuItem>
+            )}
+          </Select>
+        </FormControl>
+      </Box>
+
+    </>
+  );
+};
 const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic: boolean, showMisclassifiedOnly: boolean}) => {
   const { workflowsTable, comparativeModelInstance } = useAppSelector(
     (state: RootState) => state.monitorPage,
@@ -21,6 +118,30 @@ const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic:
   );
   const dispatch = useAppDispatch();
 
+  const [options, setOptions] = useState<string[]>([]);
+    const [xAxisOption, setXAxisOption] = useState<string>('');
+    const [yAxisOption, setYAxisOption] = useState<string>('');
+    useEffect(() => {
+  const instanceStates = Object.values(comparativeModelInstance);
+  const dataAvailable = instanceStates.find(d => d?.data?.length);
+  if (dataAvailable?.data) {
+    const firstItem = dataAvailable.data[0];
+    setOptions(Object.keys(firstItem));
+    // Optional: Set default X/Y
+    if (!xAxisOption) setXAxisOption(Object.keys(firstItem)[0]);
+    if (!yAxisOption) setYAxisOption(Object.keys(firstItem)[1] || '');
+  }
+}, [comparativeModelInstance]);
+
+    const inferFieldType = (data: TestInstance[], field: string): 'quantitative' | 'nominal' => {
+        const sample = data.find(d => d[field] !== undefined)?.[field];
+    
+        if (typeof sample === 'number') return 'quantitative';
+    
+        return 'nominal';
+      };
+   
+    
   useEffect(() => {
     if (!experimentId) return;
     selectedWorkflowIds.forEach((runId) => {
@@ -89,11 +210,6 @@ const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic:
 
       return `row-${Math.abs(hash)}`;
     };
-
-    // const confusionMatrixData = transformConfusionMatrix(dataRaw.labels, dataRaw.matrix);
-    // const maxValue = Math.max(...confusionMatrixData.map(d => d.value));
-    // const dataWithMax = confusionMatrixData.map(d => ({ ...d, __max__: maxValue }));
-
     const getVegaData = (data: TestInstance[]) => {
       return data.map((originalRow: TestInstance) => {
         const id = hashRow(originalRow);
@@ -139,14 +255,15 @@ const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic:
       },
 
       encoding: {
-        y: {
-          field: 'workclass',
-          type: 'nominal',
-        },
-        x: {
-          field: 'age',
-          type: 'quantitative',
-        },
+x: {
+  field: xAxisOption,
+  type: inferFieldType(instanceState.data ?? [], xAxisOption),
+},
+y: {
+  field: yAxisOption,
+  type: inferFieldType(instanceState.data ?? [], yAxisOption),
+},
+
         color: showMisclassifiedOnly
           ? {
             field: 'isMisclassified',
@@ -194,27 +311,11 @@ const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic:
           {
             value: 100,
           },
-        // stroke: {
-        //   condition: {
-        //     param: "highlight",
-        //     empty: false,
-        //     value: "black",
-        //   },
-        //   value: "transparent"
-        // },
-        // strokeWidth: {
-        //   condition: {
-        //     param: "highlight",
-        //     empty: false,
-        //     value: 2
-        //   },
-        //   value: 0
-        // },
         tooltip: [
           { field: 'actual', type: 'nominal', title: 'Actual' },
           { field: 'predicted', type: 'nominal', title: 'Predicted' },
-          { field: 'workclass', type: 'nominal', title: 'workclass' },
-          { field: 'age', type: 'quantitative', title: 'age' },
+          { field: xAxisOption, type: inferFieldType(instanceState.data ?? [], xAxisOption) , title: xAxisOption },
+          { field: yAxisOption, type: inferFieldType(instanceState.data ?? [], yAxisOption) , title: yAxisOption },
         ]
       },
     };
@@ -232,6 +333,18 @@ const ComparisonModelInstance = ({ isMosaic, showMisclassifiedOnly }: {isMosaic:
           isStatic={false}
           title={runId}
           sx={{ width: '100%', maxWidth: '100%' }}
+          controlPanel={<ControlPanel
+    xAxisOption={xAxisOption}
+    yAxisOption={yAxisOption}
+    setXAxisOption={setXAxisOption}
+    setYAxisOption={setYAxisOption}
+    options={options}
+    plotData={{
+      data: instanceState.data,
+      loading: instanceState.loading,
+      error: instanceState.error,
+    }}
+  />}
         />
       </Grid>
     );
