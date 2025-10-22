ff6a6473125540b3c86409f8121de57196cca5ea
Nikos Masouras <nmasouras2000@gmail.com>
2025-10-22 14:58:53 +0300

    fix: heatmap controls width updates

--- src/app/Tasks/DataExplorationTask/ChartControls/data-exploration-map-control.tsx
+++ src/app/Tasks/DataExplorationTask/ChartControls/data-exploration-map-control.tsx
@@ -196,7 +196,7 @@ const MapControls = () => {
 
         {mapType === 'heatmap' && (
           <>
-            <FormControl fullWidth>
+            <FormControl sx={{width: '50%'}}>
               <InputLabel>Weight By</InputLabel>
               <Select
                 value={
@@ -230,7 +230,7 @@ const MapControls = () => {
                   ))}
               </Select>
             </FormControl>
-            <FormControl fullWidth>
+            <FormControl sx={{width: '40%'}}>
               <ThemeProvider theme={theme}>
                 <Box display="flex" alignItems="center" gap={1}>
                   <TrackChangesIcon fontSize="small" />
