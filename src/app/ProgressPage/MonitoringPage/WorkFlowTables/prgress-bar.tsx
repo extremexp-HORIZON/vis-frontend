0722ccbe9434ba218d80ba16146c5ecd6d4c12ea
Nikos Masouras <nmasouras2000@gmail.com>
2025-12-23 12:55:48 +0200

    fix: remove percentage from status column

--- src/app/ProgressPage/MonitoringPage/WorkFlowTables/prgress-bar.tsx
+++ src/app/ProgressPage/MonitoringPage/WorkFlowTables/prgress-bar.tsx
@@ -27,7 +27,6 @@ export default function ProgressBar({ workflowStatus, workflowId } : {workflowSt
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', flexDirection: 'column', height: '100%' }}>
       <Box sx={{ display: 'flex', flexDirection: 'row', gap: 0.5 }}>
         <Typography variant="body2">{workflowStatus?.toLowerCase()}</Typography>
-        {workflowStatus === 'RUNNING' && <Typography variant="body2">{Math.floor(progressValue)}%</Typography>}
       </Box>
       <Box sx={{ width: '100%' }}>
         <LinearProgress sx={{ borderRadius: 4 }} color={color} value={progressValue} variant="determinate"/>
