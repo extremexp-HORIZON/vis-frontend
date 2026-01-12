56db2244c29e29bc859c14250b9bd5080cd59abf
Nikos Masouras <nmasouras2000@gmail.com>
2026-01-12 10:49:29 +0200

    fixed: metric comparison fetch bug

--- src/app/ProgressPage/MonitoringPage/ComparativeAnalysis/comparison-metrics-charts.tsx
+++ src/app/ProgressPage/MonitoringPage/ComparativeAnalysis/comparison-metrics-charts.tsx
@@ -96,15 +96,20 @@ const ComparisonMetricsCharts: React.FC = () => {
     previousSelectedRef.current = currentSelected;
 
     added.forEach(workflowId => {
-      if (workflowId in selectedWorkflowsMetrics.data) return;
+      const availableMetricNames =
+        workflows.data.find(wf => wf.id === workflowId)?.metrics?.map(m => m.name) || [];
 
-      const workflowMetrics = workflows.data.find(wf => wf.id === workflowId)?.metrics?.map(m => m.name) || [];
-      const metricNames = workflowMetrics
+      const comparativeMetrics = comparativeVisibleMetrics
         .filter(m => m !== 'rating')
-        .filter(m => comparativeVisibleMetrics.includes(m));
+        .filter(m => availableMetricNames.includes(m));
 
-      if (metricNames?.length) {
-        dispatch(fetchWorkflowMetrics({ experimentId, workflowId, metricNames }));
+      const existingWorkflows = selectedWorkflowsMetrics.data?.[workflowId] || [];
+      const existingNames = new Set(existingWorkflows.map(m => m.name));
+
+      const missingMetrics = comparativeMetrics.filter(m => !existingNames.has(m));
+
+      if (missingMetrics.length) {
+        dispatch(fetchWorkflowMetrics({ experimentId, workflowId, metricNames: missingMetrics }));
       }
     });
   }, [workflowsTable.selectedWorkflows, workflowsTable.initialized]);
